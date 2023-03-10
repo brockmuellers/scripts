ae1c3cdd979c47176a9e6e100a59326de851e2fd
Sara Brockmueller <sara@usesilo.com>
2023-03-09 17:18:06 -0800

    update trails; partly implement support for humboldt-toiyabe

--- trails/run.py
+++ trails/run.py
@@ -8,27 +8,27 @@ import os
 @click.command()
 @click.option('--force_reload', default=False, help='Force reload cached results (auto-reloads each hour).')
 @click.option('--no_reload', default=False, help='Use the most recently loaded result (no reload).')
-@click.option('--year', default=2022, help='Year to get results for.')
+@click.option('--year', default=2023, help='Year to get results for.')
 @click.option('--print_mode', default="default", help='Print mode for results.')
 @click.option('--trailhead', default=None, help='Specific trailhead to load results for.')
 @click.option('--date', default=None, help='Specific date to load results for.')
 @click.option('--weekends', default=False, help='Only load results for weekend trips (Fri/Sat).')
-def run(force_reload: bool, no_reload: bool, year: int, print_mode: str, trailhead: str, date: str, weekends: bool): 
+@click.option('--park', default=None, help='Load results for only this park (inyo, humboldt_toiyabe).')
+def run(force_reload: bool, no_reload: bool, year: int, print_mode: str, trailhead: str, date: str, weekends: bool, park: str): 
+
+    # note that this works with all results for both inyo and humboldt-toiyabe
+    # todo humboldt-toiyabe support is not complete
 
     if not no_reload:
         # directory for results: ./results/{year of results}/{date and hour results were pulled}
-        # results are stored as: {directory for results}/{month as int}.json
+        # results are stored as: {directory for results}/{national forest name}/{month as int}.json
         now_str = datetime.now().strftime("%Y-%m-%d-%H")
         basepath_for_now = f'results/{year}/{now_str}'
-        existing_basepaths = glob.glob(f'results/{year}/*')
 
         # if basepath doesn't exist (e.g. has not reloaded within the hour), reload
-        if basepath_for_now not in existing_basepaths:
+        if basepath_for_now not in glob.glob(f'results/{year}/*'):
             print("no results loaded within the last hour")
             os.makedirs(basepath_for_now)
-            force_reload = True
-        elif len(glob.glob(f'{basepath_for_now}/*')) != 12:
-            print("not all results were loaded")
             force_reload = True
 
         if force_reload:
