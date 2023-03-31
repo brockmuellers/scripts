b2b5c6f4e62e137b79a106043e37667da5a9b7f2
Sara Brockmueller <sara.brockmueller@gmail.com>
2023-03-31 12:42:50 -0700

    improve weekday, min available handling

--- trails/printer.py
+++ trails/printer.py
@@ -3,43 +3,120 @@ import divisions
 import json
 import loads
 
+# weekdays
+MON = 0
+TUE = 1
+WED = 2
+THU = 3
+FRI = 4
+SAT = 5
+SUN = 6
+
 
 class FilterParams:
-	def __init__(self, results, park, trailheads, dates, months: list[int], weekend_only):
+	def __init__(self, results, park, trailheads, dates, months: list[int], weekdays: list[int], min_open: int):
 		self.results = results
 		self.park = park
 		self.trailheads = trailheads
 		self.dates = dates
 		self.months = months
-		self.weekend_only = weekend_only
+		self.weekdays = weekdays
+		self.min_open = min_open
 
 def default(params: FilterParams):
+	if params.months == None:
+		params.months = [5, 6, 7, 8, 9, 10] # exclude non-quota seasons
 	raw(params)
 
 def raw(params: FilterParams):
-	to_print = _print_filter(params, only_available=True)
-	print(json.dumps(to_print, indent=4, sort_keys=True))
+	print_results(params)
 	
 def minarets(params: FilterParams):
+	# trip with ross/wesley
 	params.months = [7, 8]
+	params.min_open = 3
+	
 	if params.park == "inyo":
 		params.trailheads = [
 			"Shadow Creek",
 			"River Trail",
 			"High Trail",
 			"Rush Creek", # safest choice for late season
-			"Minaret Lake",
-			"John Muir Trail North of Devils Postpile"
+			"Minaret Lake"
 		]
 	else:
 		return
 	
-	to_print = _print_filter(params, only_available=3)
-	print(json.dumps(to_print, indent=4, sort_keys=True))
+	print_results(params)
 	
+def late_season(params: FilterParams):
+	params.min_open = 2
+	
+	params.dates = [
+		"2023-09-15",
+		"2023-09-16",
+		"2023-09-22",
+		"2023-09-23",
+		"2023-09-29",
+		"2023-09-30",
+		"2023-10-06",
+		"2023-10-07",
+		"2023-10-13",
+		"2023-10-14",
+		"2023-10-20",
+		"2023-10-21",
+	]
+	
+	if params.park == "inyo":
+		params.trailheads = [
+			# south of palisades, north has more lakes+space, south is less crowded?
+			# at 7.7k', better in Oct, 10+ mi?
+			"Big Pine Creek North Fork", # (short/long oct)
+			"Big Pine Creek South Fork", # (short/long oct)
+			
+			# leads to royce lakes, 7.4k' so better in Oct, 17 mi
+			"Pine Creek", # (long oct)
+			
+			# rock creek area, 9.6k' so better in sep
+			# goes up from rock creek past ruby lake to pioneer basin
+			"Mono Pass", # 18 mi (long sep)
+			"Little Lakes Valley", # easy and pretty, 10 mi (short sep)
+			
+			"Convict Creek" # late season only! river crossing, <10mi, 7.8k' (short oct)
+		]
+		print_results(params, "aspens inyo")
+		
+		params.trailheads = [
+			# no aspens, but still seems nice in late season
+			# (long sep or early oct), ~30 mi
+			
+			"Shadow Creek",
+			"River Trail",
+			"High Trail",
+			"Rush Creek", # safest choice for late season
+			"Minaret Lake"
+		]
+		print_results(params, "minarets inyo")
+		
+	elif params.park == "humboldt_toiyabe":
+		params.trailheads = [
+			"Robinson Creek",
+			"Buckeye Creek",
+			"Virginia Lakes"
+		]
+		return # skip me until I've done more research, but these should be easy + nice aspens
+	elif params.park == "yosemite":
+		# find some good short late season hikes from tuolumne; is shorter drive than inyo
+		return
+	elif params.park == "seki":
+		# permit season does end eventually, so maybe don't need to search for this?
+		return
+	else:
+		return
 
 def summer(params: FilterParams):
-	params.months = [7, 8, 9]
+	params.min_open = 2
+	params.weekdays = val_or_default(params.weekdays, [THU, FRI, SAT])
 	
 	if params.park == "inyo":
 		params.trailheads = [
@@ -52,23 +129,25 @@ def summer(params: FilterParams):
 			"High Trail",
 			"Rush Creek", # safest choice for late season
 			"Minaret Lake",
-			"John Muir Trail North of Devils Postpile",
 			# leads to royce lakes
 			"Pine Creek",
 			# goes up from rock creek past ruby lake to pioneer basin
 			"Mono Pass",
-			# late season only! river crossing
-			"Convict Creek",
-			"Little Lakes Valley", # easy and pretty
-			"Kearsage Pass", # some cool pinnacles, pretty far south
+			# short and easy and pretty
+			"Little Lakes Valley", 
+			# some cool pinnacles, pretty far south
+			"Kearsage Pass",
 		]
+		# excluding june because snow
+		params.months = [7, 8]
 		
 	elif params.park == "yosemite":
 		params.trailheads = [
-			# for grand canyon -> waterwheel
+			# for grand canyon -> waterwheel; white wolf direction preferred, ~30 mi
 			"White Wolf->Pate Valley",
 			"Glen Aulin->Cold Canyon/Waterwheel (pass through)"
 		]
+		params.months = [7, 8]
 		
 	elif params.park == "humboldt_toiyabe":
 		params.trailheads = [
@@ -101,12 +180,31 @@ def summer(params: FilterParams):
 	else:
 		return
 
-	to_print = _print_filter(params, only_available=2)
-	print(json.dumps(to_print, indent=4, sort_keys=True))
+	print_results(params)
 	
 
 def anniversary(params: FilterParams):
+	params.min_open = 2
+	
+	all_dates = [
+		"2023-06-09",
+		"2023-06-10",
+		"2023-06-15",
+		"2023-06-16",
+		"2023-06-17",
+		"2023-06-22",
+		"2023-06-23",
+		"2023-06-24",
+		"2023-06-30",
+		"2023-07-01"
+	]
+	best_dates = [
+		"2023-06-16",
+		"2023-06-17"
+	]
+	
 	trailheads = []
+	backup_trailheads = []
 	if params.park == "inyo":
 		trailheads = [
 			# stuff that is okay early season
@@ -138,9 +236,8 @@ def anniversary(params: FilterParams):
 			"Mono Pass"
 		]
 	elif params.park == "yosemite":
-		trailheads = []
 		backup_trailheads = [
-			# for grand canyon -> waterwheel
+			# for grand canyon -> waterwheel; tioga road may not be open
 			"White Wolf->Pate Valley",
 			"Glen Aulin->Cold Canyon/Waterwheel (pass through)"
 		]
@@ -153,64 +250,49 @@ def anniversary(params: FilterParams):
 		backup_trailheads = [
 			"Virginia Lakes"
 		]
+		# skip for now, the mono creek permit is good enough
+		return 
 	elif params.park == "sierra":
 		trailheads = [
 			"Mono Creek"
 		]
-		backup_trailheads = [
-		]
+		# already got the mono creek permit, so skip this
+		return
 	else:
 		return
 
-	all_dates = [
-		"2023-06-01",
-		"2023-06-02",
-		"2023-06-03",
-		"2023-06-09",
-		"2023-06-09",
-		"2023-06-10",
-		"2023-06-15",
-		"2023-06-16",
-		"2023-06-17",
-		"2023-06-22",
-		"2023-06-23",
-		"2023-06-24"
-	]
-	best_dates = [
-		"2023-06-16",
-		"2023-06-17"
-	]
-
-	params.summer = False # overwrite because June 2023 will apparently not be summer
-
 	# ideal trailheads, only with the ideal dates
-	print("BEST")
-	params.trailheads = trailheads
-	params.dates = best_dates
-	to_print = _print_filter(params, only_available=2)
-	print(json.dumps(to_print, indent=4, sort_keys=True))
-	# the ideal trailhead, with all dates
-	print("---------------------")
-
-	print("MORE DATES")
-	params.trailheads = trailheads
-	params.dates = all_dates
-	to_print = _print_filter(params, only_available=2)
-	print(json.dumps(to_print, indent=4, sort_keys=True))
-	# backup trailheads, with ideal dates
-	print("---------------------")
-
-	print("MORE TRAILS")
-	params.trailheads = backup_trailheads
-	params.dates = best_dates
-	to_print = _print_filter(params, only_available=2)
-	print(json.dumps(to_print, indent=4, sort_keys=True))
+	if len(trailheads) > 0:
+		params.trailheads = trailheads
+		params.dates = best_dates
+		print_results(params, "BEST " + params.park)
 	
-def _print_filter(params: FilterParams, only_available=0):
+	# the ideal trailhead, with all dates
+	if len(trailheads) > 0:
+		params.trailheads = trailheads
+		params.dates = all_dates
+		print_results(params, "MORE DATES " + params.park)
+	
+	# backup trailheads, with ideal dates
+	# skip for now, we have a backup
+	if False:
+	#if len(backup_trailheads) > 0:
+		params.trailheads = backup_trailheads
+		params.dates = best_dates
+		print_results(params, "MORE TRAILS " + params.park)
+	
+def print_results(params: FilterParams, label: str = ""):
+	if label == "":
+		label = params.park
+	label = label + ": "
+	print(label, json.dumps(_print_filter(params), indent=4, sort_keys=True))
+	
+def _print_filter(params: FilterParams):
 	results = params.results
 	dates = params.dates
 	trailheads = params.trailheads
-	weekend_only = params.weekend_only
+	weekdays = params.weekdays
+	only_available = params.min_open
 
 	divs = divisions.load_divisions()
 	divs_by_name = divs.id_by_name()
@@ -218,19 +300,20 @@ def _print_filter(params: FilterParams, only_available=0):
 
 	to_print = {}
 
+	# iterate through all results and print if it matches date/trail filters
 	for date_key, date_vals in results.items():
 		should_print_date = dates == [None] or date_key in dates
-		if should_print_date and weekend_only:
-			should_print_date = date_is_weekend(date_key)
+		if should_print_date and weekdays:
+			should_print_date = _date_in_weekdays(date_key, weekdays)
 		if should_print_date and params.months != None:
-			should_print_date = date_in_months(date_key, params.months)
+			should_print_date = _date_in_months(date_key, params.months)
 
 		for trail_id, trail_vals in date_vals.items():
 			should_print_trail = trailheads == [None] or divs_by_id[trail_id] in trailheads
 
 			if should_print_date and should_print_trail:
 				trail_name = divs_by_id[trail_id]
-				label = trail_name + " " + date_key + " " + time_key_to_weekday(date_key)
+				label = trail_name + " " + date_key + " " + _time_key_to_weekday(date_key)
 				if trail_vals["remaining"] < only_available:
 					continue
 				if trail_vals["remaining"] > 100000:
@@ -242,19 +325,19 @@ def _print_filter(params: FilterParams, only_available=0):
 	
 	return to_print
 
-def time_key_to_datetime(key: str) -> datetime:
+def _time_key_to_datetime(key: str) -> datetime:
 	return datetime.strptime(key, '%Y-%m-%d')
 
-def time_key_to_weekday(key: str) -> str:
-	return time_key_to_datetime(key).strftime('%a')
+def _time_key_to_weekday(key: str) -> str:
+	return _time_key_to_datetime(key).strftime('%a')
 
-# for entry dates F, S
-def date_is_weekend(key: str) -> bool:
-	return time_key_to_datetime(key).weekday() in [4, 5]
+def _date_in_weekdays(key: str, days: list[int]) -> bool:
+	return _time_key_to_datetime(key).weekday() in days
 	
-def date_in_months(key: str, months: list[int]) -> bool:
-	return time_key_to_datetime(key).month in months
+def _date_in_months(key: str, months: list[int]) -> bool:
+	return _time_key_to_datetime(key).month in months
 
-	
+def val_or_default(val, default):
+	return val if val else default
 	
 	
